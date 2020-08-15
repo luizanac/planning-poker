0947fc1c869ccf01f24ada5b7e0532090d2c1eb1
luizanac <luizanacletozuchinali@gmail.com>
2020-08-15 06:54:43 -0300

    project configurations and partial development

---
+++ Shared/Entities/Card.cs
@@ -0,0 +1,12 @@
+﻿namespace PlanningPoker.Shared.Entities
+{
+    public class Card
+    {
+        public string Value { get; private set; }
+
+        public Card(string value)
+        {
+            Value = value;
+        }
+    }
+}
